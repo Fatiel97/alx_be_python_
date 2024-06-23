f3f16acb32e5adf8b5ecc57cb5484cdfc4bb0733
fati <f.elhoudaigui97@gmail.com>
2024-06-23 05:07:54 -0700

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Calculate the sum
+Addition_result = number1 + number2
+
+# Calculate the difference
+Subtraction_result = number1 - number2
+
+# Calculate the product
+Multiplication_result = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {Addition_result}")
+print(f"Subtraction of {number1} and {number2} is {Subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {Multiplication_result}")
