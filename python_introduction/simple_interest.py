968891422102e085fae7ca1964e681d7806c9515
fati <f.elhoudaigui97@gmail.com>
2024-06-23 05:24:21 -0700

    second commit

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Define the variables
+principal = 1000       # representing $1000
+rate = 0.05                # representing 5% annual interest rate
+time = 3                    # representing  3 years
+
+
+# Calculate  the simple interest using the formula (I = P * R * T)
+interest = principal * rate *  time
+
+# Print the result
+print(f"The simple interest is : {interest}")
